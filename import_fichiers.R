bdcc336c8dd86a506b52b63be0267dc4deeb250b
Guillaume <gallard.git@gmail.com>
2023-01-18 16:08:22 +0100

    import

---
+++ import_fichiers.R
@@ -0,0 +1,7 @@
+#Visualisation des fichiers
+joueurs <- read.csv2("data/player_overviews_unindexed_csv.csv",sep = ",")
+tournois <- read.csv2("data/tournaments_1877-2017_unindexed_csv.csv",sep = ",")
+match_stats_2017 <- read.csv2("data/match_stats_2017_unindexed_csv.csv",sep = ",")
+
+match_stats_1991_2016 <- read.csv2("data/match_stats_1991-2016_unindexed_csv.csv",sep = ",")
+
