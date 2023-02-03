9aac6a723a485a6d584dc5359f435aa84aa8461e
Jean Sebban <sebbanjean@gmail.com>
2023-02-03 10:43:56 +0100

    modif du programme : simplification

--- constitution_base_1.R
+++ constitution_base_1.R
@@ -1,48 +1,37 @@
 library(dplyr)
 library(stringr)
 
-#1 - Importation des fichiers
+#1 - Importation des fichiers-----
 
-joueurs <- read.csv2("data/player_overviews_unindexed_csv.csv",sep = ",")
-tournois <- read.csv2("data/tournaments_1877-2017_unindexed_csv.csv",sep = ",")
+#1-1 Directement avec les liens URL--------
+match_stats_2017 <- read.csv2("https://datahub.io/sports-data/atp-world-tour-tennis-data/r/match_stats_2017_unindexed.csv",sep = ",")
+match_stats_1991_2016 <- read.csv2("https://datahub.io/sports-data/atp-world-tour-tennis-data/r/match_stats_1991-2016_unindexed.csv",sep = ",")
+match_scores_1991_2016 <- read.csv2("https://datahub.io/sports-data/atp-world-tour-tennis-data/r/match_scores_1991-2016_unindexed.csv",sep = ",")
+match_scores_2017 <- read.csv2("https://datahub.io/sports-data/atp-world-tour-tennis-data/r/match_scores_2017_unindexed.csv",sep = ",")
+joueurs <- read.csv2("https://datahub.io/sports-data/atp-world-tour-tennis-data/r/player_overviews_unindexed.csv",sep = ",")
+tournois <- read.csv2("https://datahub.io/sports-data/atp-world-tour-tennis-data/r/tournaments_1877-2017_unindexed.csv",sep = ",")
+# classements_1973_2017 <- read.csv2("https://datahub.io/sports-data/atp-world-tour-tennis-data/r/rankings_1973-2017.csv",sep = ",") #Fichier lourd
 
-#matchs stats
-match_stats_2017 <- read.csv2("data/match_stats_2017_unindexed_csv.csv",sep = ",")
-match_stats_1991_2016 <- read.csv2("data/match_stats_1991-2016_unindexed_csv.csv",sep = ",")
-#Fusion des deux fichiers
+#1-2 Si Les fichiers plats sont stockés sur le disque dur
+# match_stats_2017 <- read.csv2("data/match_stats_2017_unindexed_csv.csv",sep = ",")
+# match_stats_1991_2016 <- read.csv2("data/match_stats_1991-2016_unindexed_csv.csv",sep = ",")
+# joueurs <- read.csv2("data/player_overviews_unindexed_csv.csv",sep = ",")
+# tournois <- read.csv2("data/tournaments_1877-2017_unindexed_csv.csv",sep = ",")
+# match_scores_1991_2016 <- read.csv2("data/match_scores_1991-2016_unindexed_csv.csv",sep = ",")
+# match_scores_2017 <- read.csv2("data/match_scores_2017_unindexed_csv.csv",sep = ",")
+# classements_1973_2017 <- read.csv2("data/rankings_1973-2017_csv.csv",sep = ",")
+
+
+
+#Fusion des fichiers sur les matches de 1991 à 2017
 match_stats_1991_2017 <- match_stats_1991_2016 %>% bind_rows(match_stats_2017)
-
-
-# rm(match_stats_2017,match_stats_1991_2016)
-#match scores
-match_scores_1991_2016 <- read.csv2("data/match_scores_1991-2016_unindexed_csv.csv",sep = ",")
-match_scores_2017 <- read.csv2("data/match_scores_2017_unindexed_csv.csv",sep = ",")
 match_scores_1991_2017 <- match_scores_1991_2016 %>% bind_rows(match_scores_2017)
 # rm(match_scores_2017,match_scores_1991_2016)
+# rm(match_stats_2017,match_stats_1991_2016)
 
 
-#Classements de 1973 à 2017
-classements_1973_2017 <- read.csv2("data/rankings_1973-2017_csv.csv",sep = ",")
+#2- Ajout d'une variable d'ordre des matches--------
 
-#2 Repérer les informations manquantes sur les matchs (des deux côtés)--
-
-# matchs_manquants_1 <- match_scores_2017 %>% anti_join(match_stats_2017,by="match_id")
-# matchs_manquants_2 <- match_stats_2017 %>% anti_join(match_scores_2017,by="match_id")
-
-# Classement des matchs du plus récent au plus vieux
-
-#2- Classement des matchs--------
-#Du plus vieux au plus récent
-
-#annee (plus c'est grand, plus c'est récent)
-#numero d'ordre tournoi (plus c'est grand, plus c'est récent)
-#classer les matchs de qualification (plus c'est grand, plus c'est vieux)
-#classer les matchs du tableau principal (plus c'est grand, plus c'est vieux)
-
-#annees : on reprend les annéees (de 0 à ...) : de 1991 à 2017
-#tournoi : on reprend les numéros de tournois
-#matchs de qualifs  : on inverse  numérotation (QS000)
-#matchs principaux : on inverse la numérotation (MS000)
 
 matchs_ordonnes <- match_stats_1991_2017 %>% 
   mutate(annee = as.numeric(substr(match_id,1,4)),
@@ -52,17 +41,8 @@ matchs_ordonnes <- match_stats_1991_2017 %>%
   rename(numero_ordre = "1:nrow(match_stats_1991_2017)")
   
 #Il n'y a pas toujours les données des matchs de qualification
-#Certainement un manque de données
 
-
-#3 Repérer les informations manquantes sur les matchs (des deux côtés)--
-
-# matchs_manquants_1 <- match_scores_1991_2017 %>% 
-#   anti_join(match_stats_1991_2017,by="match_id")
-# matchs_manquants_2 <- match_stats_1991_2017 %>% 
-#   anti_join(match_scores_1991_2017,by="match_id")
-
-# 4 Filtrer les matches pour lesquels on ne retrouve pas les scores
+# 3 Filtre des matches pour lesquels on ne retrouve pas les scores
 matchs_ordonnes_filtres <- matchs_ordonnes %>% semi_join(match_scores_1991_2017,by="match_id")
 #Seulement 17 matches pour lesquels on a pas les scores
 
@@ -93,53 +73,16 @@ don <- don %>%
          loser_height = height_cm.y) %>% 
   select(match_id,numero_ordre,winner_player_id,winner_weight,winner_height,winner_age,loser_player_id,loser_weight,loser_height,loser_age)
 
+#Création d'un id
 
-#Compteur de matchs par joueur par année
 
-
-# annee_debut <- 1991
-# annee_fin <- 2017
-
+# Ajout d'un compteur de matchs par joueur par année
 toto1 <- unique(don$winner_player_id) %>% as.data.frame()
 toto2 <- unique(don$loser_player_id)%>% as.data.frame()
-# toto <- toto1 %>% anti_join(toto2,by=c("." = ".")) %>% rename("player_id" = ".")
-
-toto <- toto1 %>% full_join(toto2,by=c("." = ".")) %>% rename("player_id" = ".")
-# don$nb_matches_winner <- "" %>% as.numeric()
-# don$nb_matches_loser <- "" %>% as.numeric()
-
-# #1ère méthode
-# for (i in toto$player_id){
-#   a <- which(don$winner_player_id == i)
-#   b <- which(don$loser_player_id == i)
-#   c <- append(a,b)
-#   c <- sort(c)  #toutes les matchs joués par le joueur i
-#   don[c,"nb_matches"] <- 0:(length(c)-1)
-#   don$nb_matches_ref[c] <- i
-# 
-# }
+toto <- toto1 %>% full_join(toto2,by=c("." = ".")) %>% rename("player_id" = ".") #%>% sample_n(100)
 
 
-# 2eme méthode qui marche mieux
-
-# b <- list()
-# for (i in toto$player_id[1]){
-#   a <- don %>% 
-#     filter(winner_player_id == toto$player_id[1] | loser_player_id == toto$player_id[1])
-#   # a <-   mutate(nb_matches = 0:length(a))
-#   n <- nrow(a)
-#   a <- a %>% mutate(nb_matches = 0:(n-1))
-#   a <- a %>% mutate(winner_nb_matches = case_when(
-#     winner_player_id == toto$player_id[1] ~ as.character(nb_matches)),
-#                     loser_nb_matches= case_when(
-#                       loser_player_id == toto$player_id[1] ~ as.character(nb_matches)))
-#   assign(i,a) 
-#  
-# b[i] <- list(a)
-#   rm(a)
-#  
-# }
-
+#Création d'un objet qui contient autant d'éléments que de joueurs différents dans l'ensemble des matchs
 b <- list()
 for (i in toto$player_id){
   print(i)
