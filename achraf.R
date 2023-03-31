abfa3929b98111de1b2af67764713d8334002468
Jean Sebban <sebbanjean@gmail.com>
2023-03-31 16:14:20 +0200

    maj du programme d'achraf

--- achraf.R
+++ achraf.R
@@ -1,13 +1,22 @@
-
+library(dplyr)
+library(visNetwork)
+library(rpart)
+library(sparkline)
 
 data <- readRDS("./data/don_sansNA.rds") %>% 
   select(-starts_with("match")) %>% 
   mutate_if(is.character, as.factor)
 
-model <- rpart(target ~ ., data = dplyr::sample_n(data, 10000), cp = 0.001)
+skimr::skim(data)
 
+model <- rpart(target ~ ., 
+               data = dplyr::sample_n(data, 10000), 
+               cp = 0.0016,
+               maxcompete = 3, #montre les variables proches de celle choisie
+               maxsurrogate=1)#permet de traiter les valeurs manquantes
+summary(model)
 
-visNetwork::visTree(model, fallenLeaves = T, height = "1000px")
+visNetwork::visTree(model, fallenLeaves = TRUE, height = "1000px")
 
 small_data <-  dplyr::sample_n(data, 10000)
 
